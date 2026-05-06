c96b5a043a063c19c980e00fa5cd8dbc44dd11ac
Ken Cheng <kencheng.kc8@gmail.com>
2026-05-06 13:18:34 -0400

    Changed slit opening

--- apps/frontend/components/Splash.tsx
+++ apps/frontend/components/Splash.tsx
@@ -57,16 +57,7 @@ export function Splash() {
           className="quantiv-splash-layer quantiv-splash-open-q"
           draggable={false}
         />
-        <div className="quantiv-splash-slit-half quantiv-splash-slit-upper">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="/brand/QuantivSplashSlit.png"
-            alt=""
-            className="quantiv-splash-layer"
-            draggable={false}
-          />
-        </div>
-        <div className="quantiv-splash-slit-half quantiv-splash-slit-lower">
+        <div className="quantiv-splash-slit-arc">
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             src="/brand/QuantivSplashSlit.png"
